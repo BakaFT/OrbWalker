b8a0432a3ac9de148c98f20473f6679f2667a95e
BakaFT <bakaft@outlook.com>
2024-01-13 02:00:18 +0800

    bruh

--- OrbWalker/agent/script.cpp
+++ OrbWalker/agent/script.cpp
@@ -70,7 +70,7 @@ bool script::is_attacking() { return game_time < last_attack_time + Object::self
 
 void script::idle() {
   const auto buffs = Object().self()->buffs();
-  for (size_t i; i < buffs.size(); i++) {
+  for (size_t i = 0; i < buffs.size(); i++) {
       MessageBoxA(NULL, buffs[i]->name().c_str(), "TEST", MB_OK);
   }
   if (!is_attacking()) do_action(Move2Mouse);
